7bcbf86249473bc3a34acf851eacd50f15eeb7cb
yahya-Tech59 <yahyekade790@gmail.com>
2024-01-10 11:30:17 +0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+            headerShadowVisible: false,
+          },
+        }}
+      />
+    </SafeAreaView>
+  );
+}
